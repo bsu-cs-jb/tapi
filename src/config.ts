27c598bf06ad220fcb3dd2d535f086bee22c6900
Jonathan Branam <jonathan.branam@gmail.com>
2023-11-05 22:28:43 -0500

    Imports from grading package.

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,15 @@
-import 'dotenv/config'
+import "dotenv/config";
+
+import { BaseConfig, BaseConfigImpl, getConfig } from "grading";
 
 interface Config extends BaseConfig {
   APP_PORT: string;
 }
 
 class ConfigImpl extends BaseConfigImpl {
-  public get APP_PORT():string {
+  public get APP_PORT(): string {
     return getConfig("APP_PORT", "3000");
   }
 }
 
-export const config:Config = new ConfigImpl();
+export const config: Config = new ConfigImpl();
